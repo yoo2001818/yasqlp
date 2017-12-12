a8f7fc027f690e61d2dd8218839fb068d68625ce
Duknam Yoo <yoo2001818@gmail.com>
2017-12-13 01:36:22 +0900

    Add simple test

---
+++ src/index.test.js
@@ -0,0 +1,17 @@
+import { parse } from './parser';
+
+describe('parser', () => {
+  it('should parse basic SQL', () => {
+    expect(parse('SELECT a.b FROM a WHERE c = 5 AND b = 9;')).toEqual([
+      {
+        type: 'select',
+        columns: [['a', 'b']],
+        tables: ['a'],
+        where: [
+          { op: '=', left: ['c'], right: { value: 5 } },
+          { op: '=', left: ['b'], right: { value: 9 } },
+        ],
+      },
+    ]);
+  });
+});
